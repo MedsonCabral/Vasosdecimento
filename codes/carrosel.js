c5c58d11a834bc465413d03af2aa63fa00a75bd7
João Victor <Neguchos96@gmail.com>
2023-12-27 13:21:29 -0300

    remove file js

--- codes/carrosel.js
+++ codes/carrosel.js
@@ -1,21 +1,3 @@
-// var slideIndex = 0;
-
-// function carouselFinal() {
-//   var i;
-//   var x = document.getElementsByClassName("slide");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > x.length) {slideIndex = 1}
-//   x[slideIndex-1].style.display = "block";
-//   setTimeout(carouselFinal, 1500); // Change image every 2 seconds
-// }
-
-// carouselFinal()
-
-// TESTES
-
 var slideIndex = 1;
 showDivs(slideIndex);
 
